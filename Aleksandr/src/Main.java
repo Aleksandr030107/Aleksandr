d98afe5e51908e5c459cebdaa16fe41df7f7c4ef
Aleksandr030107 <127854790+Aleksandr030107@users.noreply.github.com>
2023-03-14 17:43:54 +0400

    Add files via upload

---
+++ Aleksandr/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String gn = "sanya vv";
+        System.out.println("!");
+        System.out.println("Hello world!");
+
+    }
+}
